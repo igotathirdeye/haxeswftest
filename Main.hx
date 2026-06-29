b9ca38cbd85befd8d83b1503f5d0e34d2aaff09b
jellou_ <liamprogames14@gmail.com>
2026-06-28 20:05:50 -0400

    Implement player shapes and movement controls
    Added player1 and player2 shapes with movement controls.

--- Main.hx
+++ Main.hx
@@ -6,7 +6,8 @@ import openfl.events.KeyboardEvent;
 import openfl.ui.Keyboard;
 
 class Main {
-    static var shape:Shape;
+    static var player1:Shape;
+    static var player2:Shape;
 
     // what.
     static var keysPressed:Map<Int, Bool> = new Map<Int, Bool>();
@@ -17,13 +18,20 @@ class Main {
         var stage = Lib.current.stage;
 
         // create a center aligned rounded gray square
-        shape = new Shape();
-        shape.graphics.beginFill(0x333333);
-        shape.graphics.drawRoundRect(0, 0, 100, 100, 10);
-        shape.x = (stage.stageWidth - 100) / 2;
-        shape.y = (stage.stageHeight - 100) / 2;
+        player1 = new Shape();
+        player1.graphics.beginFill(0xAA0000);
+        player1.graphics.drawRoundRect(0, 0, 100, 100, 10);
+        player1.x = (stage.stageWidth - 100) / 3;
+        player1.y = (stage.stageHeight - 100) / 3 * 2;
 
-        stage.addChild(shape);
+        player2 = new Shape();
+        player2.graphics.beginFill(0x0000AA);
+        player2.graphics.drawRoundRect(0, 0, 100, 100, 10);
+        player2.x = (stage.stageWidth - 100) / 3 * 2;
+        player2.y = (stage.stageHeight - 100) / 3;
+
+        stage.addChild(player1);
+        stage.addChild(player2);
 
         stage.addEventListener(KeyboardEvent.KEY_DOWN, onKeyDown);
         stage.addEventListener(KeyboardEvent.KEY_UP, onKeyUp);
@@ -60,8 +68,27 @@ class Main {
             dx = (dx / len) * speed * dt * 60;
             dy = (dy / len) * speed * dt * 60;
 
-            shape.x += dx;
-            shape.y += dy;
+            player2.x += dx;
+            player2.y += dy;
+        }
+
+        dx = 0.0;
+        dy = 0.0;
+
+        if (keysPressed.get(Keyboard.A)) dx = -1;
+        if (keysPressed.get(Keyboard.D)) dx = 1;
+        if (keysPressed.get(Keyboard.W)) dy = -1;
+        if (keysPressed.get(Keyboard.S)) dy = 1;
+
+        if (dx != 0 || dy != 0) {
+            // math
+            var len = Math.sqrt(dx*dx+dy*dy);
+
+            dx = (dx / len) * speed * dt * 60;
+            dy = (dy / len) * speed * dt * 60;
+
+            player1.x += dx;
+            player1.y += dy;
         }
     }
 }
